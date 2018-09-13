5194b1d2800e367265326c4ccce80d7a237232a3
Decklan <decklan007@gmail.com>
2018-09-12 19:36:46 -0700

    A few basic descriptions for disorders. Lorum ipsum filler on getting started page because I hate seeing the foot up in the page.

--- src/app/services/disorder.service.ts
+++ src/app/services/disorder.service.ts
@@ -27,8 +27,8 @@ export class DisorderService {
    */
   createDisorders() {
     this.disordersArray = [];
-    this.disordersArray.push(new Disorder('Attention Deficit/Hyperactivity Disorder', 'Description1'));
-    this.disordersArray.push(new Disorder('Autism Spectrum Disorder', 'Description2'));
+    this.disordersArray.push(new Disorder('Attention Deficit/Hyperactivity Disorder', 'ADHD is a highly genetic, brain-based syndrome that has to do with the regulation of a particular set of brain functions and related behaviors. These brain operations are collectively referred to as "executive functioning skills" and include important functions such as attention, concentration, memory, motivation and effort, learning from mistakes, impulsivity, hyperactivity, organization, and social skills. There are various contributing factors that play a role in these challenges including chemical and structural differences in the brain as well as genetics.'));
+    this.disordersArray.push(new Disorder('Autism Spectrum Disorder', 'Autism, or autism spectrum disorder, refers to a range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communication, as well as by unique strengths and differences. We now know that there is not one autism but many types, caused by different combinations of genetic and environmental influences.'));
     this.disordersArray.push(new Disorder('Cleft Lip and Cleft Palate', 'Description3'));
     this.disordersArray.push(new Disorder('Traumatic Brain Injury', 'Description4'));
     this.disordersArray.push(new Disorder('Down Syndrome', 'Description5'));
